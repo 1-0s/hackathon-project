99842193d88438cedb63c27212029f444f165ff6
Fagbemi Oluwaseyi <seyifagbemi211@gmail.com>
2022-07-03 08:36:16 +0100

    feat: created login and registeration features

--- components/Register/Register.jsx
+++ components/Register/Register.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { useState } from "react";
+import RegisterationForm from "./RegisterationForm";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [radioState, setRadioState] = useState("");
+  const [formState, setFormState] = useState(true);
 
-export default Register
+  const handleChange = (event) => {
+    setRadioState(event.target.value);
+  };
+
+  const btnHandler = () => {
+    if (!radioState) {
+      alert("Please click something");
+    } else {
+      setFormState(false);
+      // console.log(radioState);
+    }
+  };
+
+  return (
+    <section className=" bg-teal-100 h-[480px] grid place-items-center my-auto">
+      <div className="text-center">
+        <h1 className="mb-5 font-bold text-xl">Create an account:</h1>
+        {formState && (
+          <>
+            <p className="mb-5">Which option best fits you?</p>
+
+            <div className="flex flex-col md:flex-row md:w-[500px] md:justify-around">
+              <label htmlFor="checkbox1">
+                <input
+                  type="radio"
+                  id="checkbox1"
+                  value="professional"
+                  readOnly
+                  checked={radioState === "professional"}
+                  onChange={handleChange}
+                  className="mb-4 md:mb-0"
+                />
+                <span className="ml-2 text-pup font-bold">
+                  I am a medical professional
+                </span>
+              </label>
+
+              <label htmlFor="checkbox2">
+                <input
+                  type="radio"
+                  id="checkbox2"
+                  value="help"
+                  readOnly
+                  checked={radioState === "help"}
+                  onChange={handleChange}
+                  className=""
+                />
+                <span className="ml-2 text-pup font-bold">
+                  I need medical help
+                </span>
+              </label>
+            </div>
+
+            <button
+              className="mt-5 py-[6px] bg-pup text-sm text-white w-[80px] "
+              type="button"
+              onClick={btnHandler}
+            >
+              Continue
+            </button>
+          </>
+        )}
+        {!formState && <RegisterationForm radioState={radioState} />}
+        <p className="text-sm mt-3">
+          Already have an account?
+          <span className="text-pup cursor-pointer font-bold">
+            <Link href="/login"> Login</Link>
+          </span>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
---
+++ components/Register/RegisterationForm.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { useRef } from "react";
+import { useRouter } from "next/router";
+
+const RegisterationForm = ({ radioState }) => {
+  const router = useRouter();
+
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const emailRef = useRef();
+  const birthRef = useRef();
+  const passwordRef = useRef();
+  const formRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const firstName = firstNameRef.current.value;
+    const lastName = lastNameRef.current.value;
+    const email = emailRef.current.value;
+    const birth = birthRef.current.value;
+    const password = passwordRef.current.value;
+
+    const user_cred = {
+      radioState,
+      firstName,
+      lastName,
+      email,
+      birth,
+      password,
+    };
+
+    if (firstName && lastName && email && birth && password) {
+      console.log(user_cred);
+      router.push("/login");
+    }
+
+    firstNameRef.current.value = "";
+    lastNameRef.current.value = "";
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+    router.push("/login");
+  };
+
+  return (
+    <div className="text-center">
+      <h1 className="mb-3">Your details:</h1>
+      <form
+        className="flex flex-col items-center"
+        onSubmit={submitHandler}
+        ref={formRef}
+      >
+        <input
+          ref={firstNameRef}
+          type="text"
+          className="mb-3 w-64 md:w-80 pl-3 py-2 text-xs outline-none border-solid border-[1px] border-pup"
+          placeholder="First Name:"
+          minLength="5"
+          required
+        />
+
+        <input
+          ref={lastNameRef}
+          type="text"
+          className="mb-3 w-64 md:w-80 pl-3 py-2 text-xs outline-none border-solid border-[1px] border-pup"
+          placeholder="Last Name:"
+          minLength="5"
+          required
+        />
+        <input
+          ref={emailRef}
+          type="email"
+          className="mb-3 w-64 md:w-80 pl-3 py-2 text-xs outline-none border-solid border-[1px] border-pup"
+          placeholder="Email address:"
+          required
+        />
+        <label
+          htmlFor="date"
+          className="text-xs text-gray-500 self-start mb-2 ml-1"
+        >
+          Date of birth:
+        </label>
+        <input
+          ref={birthRef}
+          type="date"
+          id="date"
+          value="1960-01-01"
+          // min="2018-01-01"
+          max="2010-12-31"
+          className="mb-3 w-64 md:w-80 pl-3 py-2 text-xs outline-none border-solid border-[1px] border-pup"
+          required
+        />
+        <input
+          ref={passwordRef}
+          type="password"
+          className="w-64 md:w-80 pl-3 py-2 text-xs outline-none border-solid border-[1px] border-pup"
+          placeholder="Password:"
+          minLength="7"
+          required
+        />
+        <button
+          className="mt-3 py-[6px] bg-pup text-sm text-white w-[80px] "
+          type="submit"
+        >
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterationForm;
